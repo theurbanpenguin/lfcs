6b55b39c8c9d853396bd8fcac762310334f69f52
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2023-12-31 16:38:25 +0000

    Add path units

--- operating/etc/systemd/system/setexec.sh
+++ operating/etc/systemd/system/setexec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#/home/vagrant/bin/setexec.sh
+#/home/vagrant/bin/setexec.sh  Put in the vagrant use home and bin
 # Set Execute permissions where execute is not set on files
 find /home/vagrant/bin -type f ! -executable -exec chmod a+x {} \;
